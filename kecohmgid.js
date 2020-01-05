802dc9501bf3bf35bf076e727c994860642d07e8
BBBB8 <52096769+BBBB8@users.noreply.github.com>
2020-01-05 13:55:27 +0800

    Create kecohmgid.js

---
+++ kecohmgid.js
@@ -0,0 +1,1 @@
+(function () { var script = document.createElement('script'); script.src = "//jsc.mgid.com/k/e/kecoh.info.257020.js?t=" + ((d = new Date()) ? '' + d.getUTCFullYear() + d.getUTCMonth() + d.getUTCDate() + d.getUTCHours() : ''); script.async = true; document.body.appendChild(script); })();
